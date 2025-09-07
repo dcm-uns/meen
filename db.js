165a8b115e4fe9444dac46bee59a62525172bb6c
DCM <dcm@cs.uns.edu.ar>
2025-09-07 15:40:56 -0300

    Cambios menores

--- db.js
+++ db.js
@@ -5,17 +5,20 @@ const dbName = 'peliculas'
 
 let db
 
+// Incializa la conexión a la base de datos
 const init = () =>
   MongoClient.connect(connectionUrl, { useNewUrlParser: true }).then((client) => {
     db = client.db(dbName)
   })
 
+// Inserta ciegamente un item en la colección 'movies'
 const insertItem = (item) => {
   const collection = db.collection('movies')
   return collection.insertOne(item)
 }
 
-
+// Obtiene las películas de acuerdo a un criterio
+// (aqui hardcodeado, pero debería ser lo que está en el formulario)
 const getPelis = () => {
   // hardcodeado: peliculas con "Toy" en el titulo
   const filter = {
